2dee0ef8d39cc682582b046804d0b914620e1130
mariahammerstrom <maria.hammerstrom@astro.uio.no>
2015-11-12 17:23:34 +0100

    Added empty project-files.

---
+++ Project5/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <fstream>
+#include <random>
+//#include <mpi.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
