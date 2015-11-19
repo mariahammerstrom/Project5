29218fecd9c57b637ed4bed95a0b86787ce14dfe
mariekf <m.k.foss@astro.uio.no>
2015-11-19 17:39:13 +0100

    Added RK4 and VV

--- Project5/solver.cpp
+++ Project5/solver.cpp
@@ -7,57 +7,72 @@ solver::solver()
 
 }
 
-/*
-double solver::derivatives(star object1, star object2)
-{
-    double r,dv1dt[3],dv2dt[3],dx1dt[3],dx2dt[3];
-    r = distance(object1,object2);
-    for(int i=0;i<3;i++){
-        dx1dt[i] = object1.velocity[i];
-        dx2dt[i] = object2.velocity[i];
-        dv1dt[i] = -G*object1.mass*object2.mass*object1.position[i]/(r*r*r);
-        dv2dt[i] = -G*object1.mass*object2.mass*object2.position[i]/(r*r*r);
-    }
-    return dx1dt,dx2dt,dv1dt,dv2dt;
-}
+void solver::RK4(int dimension,double h, double final_time,star &object1,star &object2){
+    // 4th order Runge-Kutta solver for two coupeled first ordered ODE
 
-solver::dxdt(star object){
-    double dxdt[3];
-    for(int i=0;i<3;i++)
-        dxdt[i] = object.velocity[i];
-    return dxdt;
-}
+    double k1[dimension],k2[dimension],k3[dimension],k4[dimension]; // position
+    double l1[dimension],l2[dimension],l3[dimension],l4[dimension]; // velocity
 
-solver::dvdt(double mass,double x,double y,double z,double MassOther){
-    double dvdt[3];
-    double r = sqrt(x*x+y*y+z*z);
-    dvdt[0] = -G*mass*MassOther*x/(r*r*r);
-    dvdt[1] = -G*mass*MassOther*y/(r*r*r);
-    dvdt[2] = -G*mass*MassOther*z/(r*r*r);
-    return dvdt;
-}
+    double r,Fg; // distance and gravitational force between the two objects
 
+    for(double t=0;t<final_time;t+=h){
+        r = distance(object1,object2);
+        Fg = GravitationalForce(object1,object2);
 
-solver::RungeKutta4(double h,double final_time,double time_step,star &object1)
-{
-    double k1,k2,k3,k4;
-    for(int time=0;time<final_time;time+=time_step){
-        k1 = dvdt(mass,x,y,z,MassOther);
-        k2 = dvdt(mass,x+k1/2.,y+k1/2.,z+k1/2.,MassOther);
-        k3 = dvdt(mass,x+k2/2.,y+k2/2.,z+k2/2.,MassOther);
-        k4 = dvdt(mass,x+k3.,y+k3.,z+k3.,MassOther);
-        x += h*(k1 + 2.*(k2 + k3) + k4)/6.;
-        y += h*(k1 + 2.*(k2 + k3) + k4)/6.;
-        z += h*(k1 + 2.*(k2 + k3) + k4)/6.;
+        for(int j=0;j<dimension;j++){
+            k1[j] = h*object1.velocity[j];
+            l1[j] = -h*Fg*object1.position[j]/r; // h*dvdt(x[j],mass1,mass2);
+        }
+        for(int j=0;j<dimension;j++){
+            k2[j] = h*(object1.velocity[j]+l1[j]/2.);
+            l2[j] = -h*Fg*(object1.position[j]+k1[j]/2.)/r; // h*dvdt(x[j]+k1[j]/2.,mass1,mass2);
+        }
+        for(int j=0;j<dimension;j++){
+            k3[j] = h*(object1.velocity[j]+l2[j]/2.);
+            l3[j] = -h*Fg*(object1.position[j]+k2[j]/2.)/r; // h*dvdt(x[j]+k2[j]/2.,mass1,mass2);
+        }
+        for(int j=0;j<dimension;j++){
+            k4[j] = h*(object1.velocity[j]+l3[j]);
+            l4[j] = -h*Fg*(object1.position[j]+k3[j])/r; // h*dvdt(x[j]+k3[j],mass1,mass2);
+        }
+
+        // Update new position and velocity
+        for(int j=0;j<dimension;j++){
+            object1.position[j] += (k1[j] + 2.*(k2[j] + k3[j]) + k4[j])/6.;
+            object1.velocity[j] += (l1[j] + 2.*(l2[j] + l3[j]) + l4[j])/6.;
+        }
     }
 }
 
 
-solver::VelocityVerlet(double h,int N,double x, double vx) // not finished
-{
-    for(int i=1;i<N;i++){
-        vx =
-                x += h + vx;
+void solver::VV(int dimension,int N,double final_time,star &object1,star &object2){
+    // Velocity-Verlet
+
+    double h = final_time/((double) N);
+    double x[dimension*N]; // position
+    double v[dimension*N]; // velocity
+
+    for(int j=0;j<dimension;j++){
+        x[j] = object1.position[j];       // initial position
+        v[j] = object1.velocity[j];       // initial velocity
+        x[j+dimension] = x[j] + v[j]*h*h; // Euler
+    }
+
+    double r,Fg;
+
+    for(int i=1; i<N; i+=1){
+        r = distance(object1,object2);
+        Fg = GravitationalForce(object1,object2);
+
+        for(int j=0;j<dimension;j++){
+            x[j+(i+1)*dimension] = 2*x[j+i*dimension] - x[j+(i-1)*dimension] - h*h*Fg*x[j+i*dimension]/r;
+            v[j+i*dimension] = (x[j+(i+1)*dimension] - x[j+(i-1)*dimension])/(2*h);
+        }
+    }
+
+    // Update position and velocity
+    for(int j=0;j<dimension;j++){
+        object1.position[j] = x[dimension*N-(dimension-j)];
+        object1.velocity[j] = v[dimension*N-(dimension-j)];
     }
 }
-*/
--- Project5/solver.h
+++ Project5/solver.h
@@ -7,10 +7,8 @@ class solver
 {
 public:
     solver();
-    //double distance(star object1,star object2);
-    //double TwoBody(double t, double x);
-    //double RungeKutta4(double h,int N,double time,double x); // Runge-Kutta 4th order method
-    //double VelocityVerlet(double t,int N); // Velocity-Verlet method
+    void RK4(int dimension,double h, double final_time,star &object1,star &object2); // Runge-Kutta 4th order method
+    void VV(int dimension,int N,double final_time,star &object1,star &object2); // Velocity-Verlet method
 };
 
 #endif // SOLVER_H
