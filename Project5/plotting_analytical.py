a02c06102f348cee773d737086ac95201d319f15
mariahammerstrom <maria.hammerstrom@astro.uio.no>
2015-11-20 15:45:28 +0100

    Cleaned up analytical plot program.

--- Project5/plotting_analytical.py
+++ Project5/plotting_analytical.py
@@ -1,5 +1,3 @@
-# PROJECT 5a)
-
 """
 A program that plots the position x and velocity v as a function of time t for a box on a spring
 solved with 4th order Runge-Kutta (RK4) and Velocity-Verlet (VV), compared with the analytical solution.
@@ -13,18 +11,18 @@ from matplotlib.pyplot import rc
 rc('font',**{'family':'serif'})
 
 
-
+ 
 def read_file(filename):
-    # Input: filename for file with structure [t,x,v]
+    # Input: filename for file with structure [t,x,y,z,v_x,v_y,v_z]
     # Output: arrays of data values
     
     data = np.loadtxt(filename,unpack=True) # Read data
     
     t = data[0]            # time
-    x = data[1]            # position
-    v = data[2]            # velocity
+    x = data[1]            # position,x-direction
+    v_x = data[2]          # velocity, x-direction
     
-    return t,x,v
+    return t,x,v_x
     
 
 def plot_time(time_step):
@@ -32,15 +30,13 @@ def plot_time(time_step):
     # compared with the analytical solution.
     
     # Get data
-    #filename_verlet = '../build-Project5-Desktop_Qt_5_5_0_clang_64bit-Debug/Verlet_%.1f.txt' % time_step
-    filename_verlet = '../build-Project5-Desktop_Qt_5_5_0_clang_64bit-Debug/VV.txt'
-    filename_RK4 = '../build-Project5-Desktop_Qt_5_5_0_clang_64bit-Debug/RK.txt'
-    # filename_verlet = 'C:\Users\mariefoss\Documents\GitHub\Project5\build-Project5-Desktop_Qt_5_5_0_MinGW_32bit-Debug\VV'
-    # filename_RK4 = 'C:\Users\mariefoss\Documents\GitHub\Project5\build-Project5-Desktop_Qt_5_5_0_MinGW_32bit-Debug\RK'
+    filename_verlet = '../build-Project5-Desktop_Qt_5_5_0_clang_64bit-Debug/VV_analytic_%.3f.txt' % time_step
+    filename_RK4 = '../build-Project5-Desktop_Qt_5_5_0_clang_64bit-Debug/RK4_analytic_%.3f.txt' % time_step
+
     t_verlet,x_verlet,v_verlet = read_file(filename_verlet)
     t_RK4,x_RK4,v_RK4 = read_file(filename_RK4)
-    #print len(t_verlet),len(t_RK4)
     
+    # Calculate energies
     E_verlet = 0.5*np.array(v_verlet)**2 + 0.5*np.array(x_verlet)**2
     E_RK4 = 0.5*np.array(v_RK4)**2 + 0.5*np.array(x_RK4)**2
     E_analytic = 0.5*np.array(-np.sin(t_verlet))**2 + 0.5*np.array(np.cos(t_verlet))**2
@@ -79,51 +75,10 @@ def plot_time(time_step):
     return
 
 
-def plot_timestep(time_step_list):
-    # Function that plots the results from VV and RK4 as a function of time, 
-    # compared with the analytical solution for different time steps.
-    
-    # Define plots
-    plt.figure(1)
-    plt.title('Comparing methods (position)',size=12)
-    plt.xlabel(r'$t$',size=14)
-    plt.ylabel(r'$x$',size=14)
-    
-    plt.figure(2)
-    plt.title('Comparing methods (velocity)',size=12)
-    plt.xlabel(r'$t$',size=14)
-    plt.ylabel(r'$v_x$',size=14)
-    
-    
-    for i in range(len(time_step_list)):
-        filename_verlet = '../build-Project5-Desktop_Qt_5_5_0_clang_64bit-Debug/Verlet_%.1f.txt' % time_step_list[i]
-        # filename_verlet = 'C:\Users\mariefoss\Documents\GitHub\Project5\build-Project5-Desktop_Qt_5_5_0_MinGW_32bit-Debug\VV_%.3f.txt' % time_step_list[i]
-        t_verlet,x_verlet,v_verlet = read_file(filename_verlet)
-        
-        plt.figure(1)
-        plt.plot(t_verlet,np.cos(t_verlet),label='Analytic, dt = %.1f' % time_step_list[i])
-        plt.plot(t_verlet,x_verlet,label='Verlet, dt = %.1f' % time_step_list[i])
-        #plt.plot(t_RK4,x_RK4,label='RK4')
-        plt.legend(loc=1,prop={'size':12})
-        
-        plt.figure(2)
-        plt.plot(t_verlet,-np.sin(t_verlet),label='Analytic, dt = %.1f' % time_step_list[i])
-        plt.plot(t_verlet,v_verlet,label='Verlet, dt = %.1f' % time_step_list[i])
-        #plt.plot(t_RK4,v_RK4,label='RK4')
-        plt.legend(loc=2,prop={'size':12})
-    
-    plt.show()
-    
-    return 0
-
 def main(argv):
     # Plot results as a function of time
-    time_step = 0.2
-    plot_time(time_step)
-
-    # Plot results for different time steps
-    # time_step_list = [0.2]
-    # plot_timestep(time_step_list)
+    plot_time(time_step=0.1)
+    
 	
 if __name__ == "__main__":
     main(sys.argv[1:]) 
