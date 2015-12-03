fd2607bec94b4be092c9159877adeac09d4fe9af
mariahammerstrom <maria.hammerstrom@astro.uio.no>
2015-12-03 16:12:08 +0100

    Added orbits plots, etc.

--- Project5/plotting_analytical.py
+++ Project5/plotting_analytical.py
@@ -18,39 +18,29 @@ def read_file(filename):
     
     data = np.loadtxt(filename,unpack=True) # Read data
     
-    #t = data[0]            # time
-    #x = data[1]            # position,x-direction
-    #v_x = data[2]          # velocity, x-direction
-
-    t = data[0]
-    x = data[3]
-    v_x = data[4]
+    t = data[0]            # time
+    x = data[1]            # position,x-direction
+    v_x = data[2]          # velocity, x-direction
     
     return t,x,v_x
     
 
-def plot_time(time_step):
+def plot_time(N,time_step):
     # Function that plots the results from VV and RK4 as a function of time, 
     # compared with the analytical solution.
     
     # Get data
-    #filename_verlet = '../build-Project5-Desktop_Qt_5_5_0_clang_64bit-Debug/VV_analytic_%.3f.txt' % time_step
-    #filename_RK4 = '../build-Project5-Desktop_Qt_5_5_0_clang_64bit-Debug/RK4_analytic_%.3f.txt' % time_step
-    filename_RK4 = 'C:/Users/mariefoss/Documents/GitHub/Project5/build-Project5-Desktop_Qt_5_5_0_MinGW_32bit-Debug/analytic_RK4.txt'
+    filename_verlet = '../build-Project5-Desktop_Qt_5_5_0_clang_64bit-Debug/a_analytic_VV_%d_%.2f.txt' % (N,time_step)
+    filename_RK4 = '../build-Project5-Desktop_Qt_5_5_0_clang_64bit-Debug/a_analytic_RK4_%d_%.2f.txt' % (N,time_step)
 
-    #t_verlet,x_verlet,v_verlet = read_file(filename_verlet)
+    t_verlet,x_verlet,v_verlet = read_file(filename_verlet)
     t_RK4,x_RK4,v_RK4 = read_file(filename_RK4)
-    
-    # Calculate energies
-    #E_verlet = 0.5*np.array(v_verlet)**2 + 0.5*np.array(x_verlet)**2
-    E_RK4 = 0.5*np.array(v_RK4)**2 + 0.5*np.array(x_RK4)**2
-    E_analytic = 0.5*np.array(-np.sin(t_RK4))**2 + 0.5*np.array(np.cos(t_RK4))**2
     
     # Make plots
     plt.figure(1)
     plt.title('Comparing methods (position), time step = %.2f' % time_step,size=12)
-    plt.plot(t_RK4,np.cos(t_RK4),label='Analytic')
-    #plt.plot(t_verlet,x_verlet,label='Verlet')
+    plt.plot(t_verlet,np.cos(t_verlet),label='Analytic')
+    plt.plot(t_verlet,x_verlet,label='VV')
     plt.plot(t_RK4,x_RK4,label='RK4')
     plt.xlabel(r'$t$',size=14)
     plt.ylabel(r'$x$',size=14)
@@ -59,30 +49,38 @@ def plot_time(time_step):
     
     plt.figure(2)
     plt.title('Comparing methods (velocity), time step = %.2f' % time_step,size=12)
-    plt.plot(t_RK4,-np.sin(t_RK4),label='Analytic')
-    #plt.plot(t_verlet,v_verlet,label='Verlet')
+    plt.plot(t_verlet,-np.sin(t_verlet),label='Analytic')
+    plt.plot(t_verlet,v_verlet,label='VV')
     plt.plot(t_RK4,v_RK4,label='RK4')
     plt.xlabel(r'$t$',size=14)
     plt.ylabel(r'$v_x$',size=14)
     plt.legend(loc=2,prop={'size':12})
     plt.show()
     
+    #"""
+    # Calculate energies
+    E_verlet = 0.5*np.array(v_verlet)**2 + 0.5*np.array(x_verlet)**2
+    E_RK4 = 0.5*np.array(v_RK4)**2 + 0.5*np.array(x_RK4)**2
+    E_analytic = 0.5*np.array(-np.sin(t_verlet))**2 + 0.5*np.array(np.cos(t_verlet))**2
+    
     plt.figure(3)
     plt.title('Total energy, time step = %.2f' % time_step,size=12)
-    plt.plot(t_RK4,np.array(E_analytic)-0.5,label='Analytic')
-    #plt.plot(t_verlet,np.array(E_verlet)-0.5,label='Verlet')
-    plt.plot(t_RK4,np.array(E_RK4)-0.5,label='RK4')
+    plt.plot(t_verlet,E_analytic,label='Analytic')
+    plt.plot(t_verlet,E_verlet,label='VV')
+    plt.plot(t_verlet,E_RK4,label='RK4')
     plt.xlabel(r'$t$',size=14)
     plt.ylabel(r'$E$',size=14)
-    plt.legend(loc=1,prop={'size':12})
+    plt.legend(loc=3,prop={'size':12})
     plt.show()
+    #"""
     
     return
 
 
 def main(argv):
     # Plot results as a function of time
-    plot_time(time_step=0.1)
+    plot_time(N=100,time_step=0.05)
+    #plot_time(N=1000,time_step=0.01)
     
 	
 if __name__ == "__main__":
