474ea58311f40bdac975070bb832b1a639520740
mariahammerstrom <maria.hammerstrom@astro.uio.no>
2015-12-11 21:44:56 +0100

    Rewrote code.

--- Project5/plotting_radialprofile.py
+++ Project5/plotting_radialprofile.py
@@ -2,10 +2,11 @@
 RADIAL DENSITY PROFILE
 A program that:
     
-    1) Draws a histogram for the particle density as a function of radius,
+    1) Draws a histogram showing the radial distribution,
     2) Plots the radial profile density,
-    3) Compares the radial profile density with two radial profile density functions.
-
+    3) Compares the radial profile density with a radial profile density function,
+    4) Calculates average radial distance and standard deviation.
+    
 """
 
 import sys
@@ -13,8 +14,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import rc
 rc('font',**{'family':'serif'})
-
-import matplotlib.mlab as mlab
 
 
 def read_file(filename):
@@ -41,7 +40,7 @@ def read_file(filename):
     
 
 def radial_profile(total_stars,time_step,final,integration_points):
-    # Input: Total stars, time step, whether to print final (True) or initial (False )distribution
+    # Input: Total stars, time step, whether to print final (True) or initial (False) distribution
     # Output: Plots and statistics.
     
     # Read file
@@ -72,66 +71,90 @@ def radial_profile(total_stars,time_step,final,integration_points):
     print "Average radius = \t", mu
     print "Standard deviation = \t", stdev
     
-    # Plot histogram
-    radii = np.linspace(0.001,20,len(r))
-    volume = 4*np.pi*r**3/3.0;
-    weights = 1./volume
     
-    #"""
-    plt.figure()
-    plt.hist(r,bins=1000,weights=weights)
-    plt.xlabel('Radial distance [ly]')
-    plt.ylabel(r'Radial density $[N/$ly$^3]$')
-    #plt.xlim(0,20)
-    plt.title(title,size=12)
-    #"""
-     
+    radial_distance = np.zeros(10)
+    
+    # Calculate the number of objects within given radii, in steps of 2 ly
+    for i in range(total_stars-1):
+        if r[i] <= 2:
+            radial_distance[0] += 1
+            
+        elif r[i] > 2 and r[i] <= 4:
+            radial_distance[1] += 1
+            
+        elif r[i] > 4 and r[i] <= 6:
+            radial_distance[2] += 1
+            
+        elif r[i] > 6 and r[i] <= 8:
+            radial_distance[3] += 1
+            
+        elif r[i] > 8 and r[i] <= 10:
+            radial_distance[4] += 1
+            
+        elif r[i] > 10 and r[i] <= 12:
+            radial_distance[5] += 1
+        
+        elif r[i] > 12 and r[i] <= 14:
+            radial_distance[6] += 1
+        
+        elif r[i] > 14 and r[i] <= 16:
+            radial_distance[7] += 1
+        
+        elif r[i] > 16 and r[i] <= 18:
+            radial_distance[8] += 1
+        
+        elif r[i] > 18 and r[i] <=20:
+            radial_distance[9] += 1
 
-    """
-    # the histogram of the data
-    plt.figure()    
-    weights = 1./volume/total_stars**(-1./3) 
-    n, bins, patches = plt.hist(r, 60, normed=1, facecolor='green', alpha=0.75, weights=weights)
     
-    # Plot
-    plt.xlabel('Radial distance [ly]')
-    plt.ylabel(r'Radial density $[N/$ly$^3]$')
-    plt.xlim(0,20)
-    plt.grid(True)
-
-    plt.show()
-    """
+    # Calculate volume of shells, thickness 2 ly each
+    shells = np.zeros(10)
     
-    # Calculate radial densities
-    radii = np.linspace(0.01,20,22)
-    radii2 = np.linspace(0.01,20,1000)
-    volume = 4*np.pi*radii**3/3.0;
+    for i in range(10):
+        shells[i] = volume((i+1)*2) - volume(i*2)
     
-    r_rounded = np.around(r, decimals=0)
-    counts = np.bincount(r_rounded.astype(int))
-    counts_limited = counts[0:len(volume)]
-    
-    print len(r),len(counts),len(counts_limited),len(volume)
-    
-    no_density = counts_limited/volume
-    
+    # Calculate number densities
+    no_density = radial_distance/shells
     
     # Calculate simple fit
-    n_0 = total_stars**2
-    r_0 = total_stars**(-1./3)
-    density_fit_simple = simple_fit(n_0,r_0,radii2)
+    radii = np.linspace(0.001,20,len(no_density))
+    radii_hires = np.linspace(0.001,20,1000)
     
-    # Plot radial densities
+    n_0 = no_density[0]
+    r_0 = np.asarray([2,4,5,6])
+    
+    # Plot number density 
     plt.figure()
-    plt.plot(radii,no_density,label='Simulation')
-    plt.plot(radii2/total_stars**(-1./3),density_fit_simple/(total_stars**2),label='Simple fit')
+    plt.plot(radii/total_stars**(-1./3),no_density/(total_stars**2),'o',label='Simulation')
+    plt.plot(radii_hires/total_stars**(-1./3),simple_fit(n_0,r_0[0],radii_hires)/(total_stars**2),label=r'Simple fit, $r_0 = 2$')
+    plt.plot(radii_hires/total_stars**(-1./3),simple_fit(n_0,r_0[1],radii_hires)/(total_stars**2),label=r'Simple fit, $r_0 = 4$')
+    plt.plot(radii_hires/total_stars**(-1./3),simple_fit(n_0,r_0[2],radii_hires)/(total_stars**2),label=r'Simple fit, $r_0 = 5$')
     plt.xlabel(r'$r/N^{-1/3}$')
     plt.ylabel(r'$n(r)/N^2$')
-    plt.xlim(0,20)
+    plt.title(r'N = %d, time step = %.3f' % (total_stars,time_step),size=12)
     plt.xscale('log')
     plt.yscale('log')
     plt.legend(loc=3,prop={'size':12})
     plt.show()
+    
+    # Plot histogram
+    plt.figure()
+    plt.hist(r,bins=20)
+    plt.xlabel('Radial distance [ly]')
+    plt.ylabel(r'Radial density $[N/$ly$^3]$')
+    plt.xlim(0,20)
+    plt.title(title,size=12)
+    plt.show()
+    
+    # Plot average distance as function of N
+    #N_values = [100,200,300,400]
+    #averages = []
+    
+    #plt.figure()
+    #plt.plot(N_values,averages)
+    #plt.xlabel(r'$N$')
+    #plt.ylabel(r'$\langle r \rangle$')
+    #plt.show()
     
     return
 
@@ -142,11 +165,15 @@ def simple_fit(n_0,r_0,r):
 
 def NFW_profile(rho_0,r_0,r):
     return rho_0/(r/r_0)/(1 + r/r_0)**2
+
+def volume(R):
+    # Volume of sphere of radius R
+    return 4*np.pi*R**3/3.0
     
 
 def main(argv):
     
-    total_stars = 300
+    total_stars = 100
     time_step = 0.001
     integration_points = 10000
 
